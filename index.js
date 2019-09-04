cf397c4384edccfc63c6e84ed71a988f301b1ca8
Sander Moen <smoensander@gmail.com>
2019-09-04 14:15:09 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+const PORT = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
